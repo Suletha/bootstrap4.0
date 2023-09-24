0a241856598c2b408170b78f6eb863bbb77e73f0
Suletha <suletha.gopi@gmail.com>
2023-09-24 08:55:04 +0530

    third commit

---
+++ dom.js
@@ -0,0 +1,32 @@
+//ELEMENT INSPECTION
+ //console.dir(document);
+ //console.log(document.domain);
+// console.log(document.title);
+ //console.log(document.doctype);
+ //console.log(document.head);
+ //console.log(document.body);
+ //console.log(document.all);
+ //document.title = suletha;
+ //document.all[10].textContent = 'Hello';
+ //console.log(document.forms);
+ //console.log(document.links);
+ //console.log(document.forms[1]);
+ //console.log(document.images);
+
+ //GET ELEMENT BY ID
+ //console.log(document.getElementById('header-title'));
+
+ var headerTitle = document.getElementById('header-title');
+  //console.log(headerTitle);
+ //headerTitle.textContent = 'Hello'
+ //headerTitle.textContent = 'Suletha'
+ //console.log(headerTitle.innerText);
+ //headerTitle.innerHTML = '<h3>Hello</h3>';
+ headerTitle.style.borderBottom = 'solid 3px #000';
+
+ var header = document.getElementById('main-header');
+ //header.style.borderBottom = 'solid 3px #000';
+ 
+
+ //GET ELEMNT BY CLASS NAME
+
